b5c6bc92cb335041497515cc9b674c2317c224e7
Luke Venediger <lukev@lukev.net>
2016-04-06 15:30:51 +0200

    Dupicated a command for the second serial port.

--- shortcuts.sh
+++ shortcuts.sh
@@ -2,5 +2,6 @@
 
 alias sjs-docker-fix-network='sudo route -nv add -net 192.168.99 -interface vboxnet0'
 alias sjs-build-and-flash-latest='pushd ~/iot-src/smart.js/smartjs/platforms/esp8266; make; /Applications/FNC.app/Contents/MacOS/FNC --flash-baud-rate 115200 --flash firmware/smartjs-esp8266-last.zip --platform esp8266 --port /dev/cu.wchusbserial1410; popd'
+alias sjs-build-and-flash-latest-2='pushd ~/iot-src/smart.js/smartjs/platforms/esp8266; make; /Applications/FNC.app/Contents/MacOS/FNC --flash-baud-rate 115200 --flash firmware/smartjs-esp8266-last.zip --platform esp8266 --port /dev/cu.wchusbserial1420; popd'
 alias sjs-flash-latest='pushd ~/iot-src/smart.js/smartjs/platforms/esp8266; /Applications/FNC.app/Contents/MacOS/FNC --flash-baud-rate 115200 --flash firmware/smartjs-esp8266-last.zip --platform esp8266 --port /dev/cu.wchusbserial1410; popd'
 alias sjs-clean='pushd ~/iot-src/smart.js/smartjs/platforms/esp8266; make clean; popd'
